9d5ad6dbac3a22020c7f13adbc4c6b248cf53a3b
luigiVezzoso <luigi.vezzoso@gmail.com>
2019-04-05 16:44:51 +0200

    Initial commit

---
+++ src/com/xabaras/tomcat/encryptedHttp11Nio2Protocol.java
@@ -0,0 +1,90 @@
+package com.xabaras.tomcat;
+
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.spec.KeySpec;
+import java.util.Base64;
+
+public class encryptedHttp11Nio2Protocol extends org.apache.coyote.http11.Http11NioProtocol {
+
+    private static String secretKey = "P@ssw0rd";
+    private static String salt = "ssshhhhhhhhhhh!!!!";
+
+
+    @Override
+    public void setKeystorePass(String s) {
+        try {
+
+            byte[] decodedBytes = Base64.getDecoder().decode(s);
+            String decodedString = new String(decodedBytes);
+
+
+            String decrifata = decrypt(s,secretKey);
+
+
+            super.setKeystorePass(decrifata);
+
+        } catch (final Exception e) {
+            super.setKeystorePass("");
+        }
+    }
+
+
+    private static String encrypt(String strToEncrypt, String secret)
+    {
+        try
+        {
+            byte[] iv = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+            IvParameterSpec ivspec = new IvParameterSpec(iv);
+
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+            KeySpec spec = new PBEKeySpec(secretKey.toCharArray(), salt.getBytes(), 65536, 256);
+            SecretKey tmp = factory.generateSecret(spec);
+            SecretKeySpec secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivspec);
+            return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes("UTF-8")));
+        }
+        catch (Exception e)
+        {
+            System.out.println("Error while encrypting: " + e.toString());
+        }
+        return null;
+    }
+
+
+    private static String decrypt(String strToDecrypt, String secret) {
+        try {
+            byte[] iv = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+            IvParameterSpec ivspec = new IvParameterSpec(iv);
+
+            SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+            KeySpec spec = new PBEKeySpec(secretKey.toCharArray(), salt.getBytes(), 65536, 256);
+            SecretKey tmp = factory.generateSecret(spec);
+            SecretKeySpec secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            cipher.init(Cipher.DECRYPT_MODE, secretKey, ivspec);
+            return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
+        } catch (Exception e) {
+            System.out.println("Error while decrypting: " + e.toString());
+        }
+        return null;
+    }
+
+    public static void main(String[] args){
+
+       String encodedString = Base64.getEncoder().encodeToString(args[0].getBytes());
+        String cifrata = encrypt(args[0],secretKey);
+        String decrifata = decrypt(cifrata,secretKey);
+        System.out.println("Base64 : " + encodedString);
+        System.out.println("AES256 : " + cifrata);
+        System.out.println("AES256 : " + decrifata);
+    }
+}
